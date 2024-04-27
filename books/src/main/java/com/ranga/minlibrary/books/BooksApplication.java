96e0d3304595dfec67e4c385ac74ba6dffb37490
Ranga Bhupal Reddy Gangavarapu <ranga.gangavarapu@koerber-supplychain.com>
2024-04-27 12:30:41 +0530

    books service - adding auditing functionality for audit columns

--- books/src/main/java/com/ranga/minlibrary/books/BooksApplication.java
+++ books/src/main/java/com/ranga/minlibrary/books/BooksApplication.java
@@ -2,8 +2,10 @@ package com.ranga.minlibrary.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class BooksApplication {
 
 	public static void main(String[] args) {
