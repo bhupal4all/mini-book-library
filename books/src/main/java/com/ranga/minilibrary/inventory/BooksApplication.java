33f5a8a98c3a5e94bcab5ff14190b6d095638f09
Ranga Bhupal Reddy Gangavarapu <ranga.gangavarapu@koerber-supplychain.com>
2024-05-05 08:17:50 +0530

    application properties

--- books/src/main/java/com/ranga/minilibrary/inventory/BooksApplication.java
+++ books/src/main/java/com/ranga/minilibrary/inventory/BooksApplication.java
@@ -1,5 +1,6 @@
 package com.ranga.minilibrary.inventory;
 
+import com.ranga.minilibrary.inventory.dto.BooksContactInfoDto;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -7,10 +8,14 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties({
+        BooksContactInfoDto.class
+})
 @OpenAPIDefinition(
         info = @Info(
                 title = "Books Microservice REST API Documentation",
---
+++ books/src/main/java/com/ranga/minilibrary/inventory/dto/BooksContactInfoDto.java
@@ -0,0 +1,16 @@
+package com.ranga.minilibrary.inventory.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+import java.util.Map;
+
+@Schema(name = "Books Contact Info", description = "Books Contact Information")
+@ConfigurationProperties(prefix = "books.contact")
+public record BooksContactInfoDto(
+        String message,
+        Map<String, String> contactDetails,
+        List<String> onCallSupport
+) {
+}
