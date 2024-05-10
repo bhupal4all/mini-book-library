a797470a02ef6bbc6cebb810819eab6b959bf8b0
Ranga Bhupal Reddy Gangavarapu <ranga.gangavarapu@koerber-supplychain.com>
2024-05-10 09:44:38 +0530

    adding edge/gateway server

---
+++ gateway-server/src/test/java/com/ranga/minilibrary/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranga.minilibrary.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
